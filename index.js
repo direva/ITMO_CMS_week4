12f43c5dac5c9d3df21f25d5ab461e683051e75e
DReva <DReva@inwk.com>
2021-09-30 19:12:56 +0300

    fixed

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(bodyParser.text())
-// app.get('*', (req, res) => res.send('hello'))
 app.post('/result4/', (req, res) => {
     const xTest = req?.headers?.['x-test']
     const xBody = req?.body
     res.set('Access-Control-Allow-Origin', '*')
-    // res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
     res.set('Access-Control-Allow-Headers', '*')
     res.set('Content-Type', 'application/json')
     res.json({ message: 'direva99', "x-result": xTest, "x-body": xBody })
@@ -17,7 +16,7 @@ app.get('/result4/', (req, res) => {
     const xTest = req?.headers?.['x-test']
     const xBody = 'abc'
     res.set('Access-Control-Allow-Origin', '*')
-    // res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
     res.set('Access-Control-Allow-Headers', '*')
     res.set('Content-Type', 'application/json')
     res.json({ message: 'direva99', "x-result": xTest, "x-body": xBody })
