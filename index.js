c4f10992f47a3357f64372681fc611447b11bac8
DReva <DReva@inwk.com>
2021-09-30 17:35:33 +0300

    Added test app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.text())
+app.post('/result4/', (req, res) => {
+    const xTest = req?.headers?.['x-test']
+    const xBody = req?.body
+    res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    res.set('Access-Control-Allow-Headers', 'x-test,Content-Type,Accept,Access-Control-Allow-Headers')
+    res.set('content-type', 'application/json')
+    res.json({ message: 'direva99', "x-result": xTest, "x-body": xBody })
+})
+app.listen(8080, () => console.log('App is listening on 8080'))
